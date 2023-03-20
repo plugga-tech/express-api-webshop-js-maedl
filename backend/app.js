29e3d4373607a88fbf0ca8278cc55ac2d6d6e7b7
Max Edlund <maxedlund88@gmail.com>
2023-03-20 14:31:21 +0100

    project scaffolding, add cors

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
