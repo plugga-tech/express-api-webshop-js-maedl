13031bdacdc0becaac6cb33d64c5ef6715d36f17
Max Edlund <maxedlund88@gmail.com>
2023-03-21 18:24:11 +0100

    add order now reduces product quantity in db

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const router = express.Router();
 
 // SKAPA ORDER FÖR EN SPECIFIK USER // PRODUCTS ÄR EN ARRAY MOTSVARANDE INNEHÅLLET I KUNDVAGN
 router.post('/add', function(req, res) {
-  req.app.locals.db.collection('orders').insertOne(req.body)
-  .then(result => {
-    res.json(result);
-  })
+
+  const productsCollection = req.app.locals.db.collection('products');
+  const orderedProducts = req.body.products;
+
+  orderedProducts.forEach((orderedProduct) => {
+
+    let id = new ObjectId(orderedProduct.productId);
+    let orderedAmount = orderedProduct.quantity;
+
+    productsCollection.findOne({_id: id})
+    .then(result => {
+     let currentAmount = result.quantity;
+     
+     let newAmount = currentAmount - orderedAmount;
+
+     productsCollection.updateOne({_id: id}, {$set: {"quantity": newAmount}});
+    });
+
+  });
+
+  res.send('answer');
 });
 
 // HÄMTA ALLA ORDERS
